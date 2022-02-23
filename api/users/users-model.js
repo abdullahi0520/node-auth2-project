657d6a079f0e8a573207cfd0d9ad0c5d04e1eaf3
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-22 22:10:05 -0600

    final

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -40,6 +40,17 @@ function findBy(filter) {
       }
     ]
    */
+  /*
+  select user_id, username, role_name, password 
+  from users join roles 
+  on users.role_id = roles.role_id 
+  where users.user_id = 1
+  */ 
+
+  return db('users')
+ .join('roles', 'users.role_id', 'roles.role_id')
+ .select('user_id', 'username', 'role_name', 'password')
+ .where(filter)
 }
 
 function findById(user_id) {
@@ -53,6 +64,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id',user_id).first()
 }
 
 /**
